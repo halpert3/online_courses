fff0e1b4613886ba1846a9371cd0890cc1800f4c
Henry Alpert <halpert3@gmail.com>
2022-01-24 16:38:02 -0600

    tranforms_start.py

--- lynda-advanced_python/Exercise Files/02 BuiltIn Functions/transforms_start.py
+++ lynda-advanced_python/Exercise Files/02 BuiltIn Functions/transforms_start.py
@@ -1,12 +1,19 @@
+
 # use transform functions like sorted, filter, map
 
 
 def filterFunc(x):
-    pass
+    if x % 2 == 0:
+        return False
+    else:
+        return True
 
 
 def filterFunc2(x):
-    pass
+    if x.islower():
+        return True
+    else:
+        return False
 
 
 def squareFunc(x):
@@ -14,7 +21,15 @@ def squareFunc(x):
 
 
 def toGrade(x):
-    pass
+    if x >= 90:
+        return "A"
+    elif x > 80:
+        return "B"
+    elif x > 70:
+        return "C"
+    elif x > 65:
+        return "D"
+    return "F"
 
 
 def main():
@@ -24,12 +39,31 @@ def main():
     grades = (81, 89, 94, 78, 61, 66, 99, 74)
 
     # TODO: use filter to remove items from a list
+    odds = list(filter(filterFunc, nums))
+    print(odds)
 
     # TODO: use filter on non-numeric sequence
+    lowers = list(filter(filterFunc2, chars))
+    print(lowers)
+
+    uppers = list(filter(lambda x: x.isupper(), chars))
+    print(uppers)
 
     # TODO: use map to create a new sequence of values
+    squares = list(map(lambda x: x ** 2, nums))
+    print(squares)
+
+    # trying it to make all uppers
+    all_uppers = list(map(lambda x: x.upper(), chars))
+    print(all_uppers)
+    print(map(lambda x: x.upper(), chars))
 
     # TODO: use sorted and map to change numbers to grades
+    grades = sorted(grades)
+    letters = list(map(toGrade, grades))
+    print(grades)
+    print(letters)
+
 
 
 if __name__ == "__main__":
